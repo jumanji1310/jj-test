9a307502812f1c9cc927733693c306b9a5375248
Jun Jie Ng <ngjunjie13@gmail.com>
2024-11-08 01:20:11 +1100

    changed to custom url

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jumanji1310.github.io',
+  url: 'https://jumanji.work',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
